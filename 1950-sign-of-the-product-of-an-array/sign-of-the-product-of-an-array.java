1596183eff44d5a3da2f2609f9c242a47254ee81
Dharun48 <146981292+Dharun48@users.noreply.github.com>
2025-12-15 09:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (16.80%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arraySign(int[] num) {
+        int sign = 1;
+
+        for (int i=0;i<num.length;i++) {
+            if (num[i]==0)
+             {
+                return 0;
+            }
+            if(num[i]<0) 
+            {
+                sign=-sign;
+            }
+        }
+        return sign;
+    }
+}
